b1b0050a0d4f2e94d7336bef98401cc5701e56c9
bmolina-nyc <bruce.molina.81@gmail.com>
2017-01-29 19:52:56 -0500

    Done.

--- public/js/cars.js
+++ public/js/cars.js
@@ -1,70 +1,40 @@
 "use strict";
 
-// this is the base API url
-var urlRefresh = "/3/3"
-var baseUrl = `http://mimeocarlisting.azurewebsites.net/api/cars/${urlRefresh}`;
-var $newDiv = $("<div class='row'>`${urlRefresh}`</div>")
+var pageNumber = 3;
+var baseUrl = "http://mimeocarlisting.azurewebsites.net/api/cars/";
 
- 
-// $( ".row" ).last().($newdiv1);
-function formatCars(carsJSON) {
-  // this function shold return a string of properly formatted html
-  // refer to app/views/index.erb lines 16 - 22 for an example of how
-  // to format three cars, each in a div with a class "col-md-4", in a 
-  // div with a class "row"
+function formatCars(cars) {
+  var html = "<div class=\"row\">";
+  $.each(cars, function(index, car) {
+    html += "<div class=\"col-md-4 car\">";
+    html += "<h2>" + car.Make + "</h2>";
+    html += "<p><strong>Model:</strong> " + car.Model + "</p>";
+    html += "<p><strong>Year:</strong> " + car.Year + "</p>";
+    html += "</div>";
+  });
+  html += "</div>"
+  return html;
 }
 
-    // <div class="col-md-4 car">
-    //         <h2>Chevrolet</h2>
-    //         <p><strong>Model:</strong> Tahoe</p>
-    //         <p><strong>Year:</strong> 2012</p>
-    //       </div>
-    //       <div class="col-md-4 car">
-
-
-function addCarsToDOM(carsJSON) {
-  // this function should pass carsJSON to formatCars() and then 
-  // add the resulting HTML to the div with an id of "cars"
-  var i = 0 
-  for (i = 0; i < carsJSON.length; i++){
-    formatCars(carsJSON[i])
-  }
+function addCarsToDOM(cars) {
+  console.log(cars)
+  var carsHTML = formatCars(cars);
+  $("#cars").append(carsHTML);
 }
-
-// the object in the api call
-// [
-//   {
-//     "Make": "Chevrolet",
-//     "Model": "Tahoe",
-//     "Year": "2012"
-//   },
-//   {
-//     "Make": "Toyota",
-//     "Model": "Camry",
-//     "Year": "2002"
-//   },
-//   {
-//     "Make": "Mercedes-Benz",
-//     "Model": "E-Class",
-//     "Year": "1998"
-//   }
-// ]
-// [6] pry(main)> cars[0][:Make], cars[0][:Model], cars[0][:Year]
-// => "Chevrolet" , "Tahoe", "2012"
 
 function fetchJSON() {
-  // this function will make the ajax call
-  // on success of the ajax call, it will pass the returned data
-  // to addCarsToDOM()
-  $.ajax({
-    url: baseUrl,
-    method: 'get',
-    dataType: 'jsonp',
-    success: function(data){
-      addCarsToDOM(data);
-    } 
-  }).fail(function(){
-      console.log("Bad link provided");
-    });
-}
+  var url = baseUrl + pageNumber + "/3";
+  pageNumber += 1;
 
+  $.ajax({
+    url: url,
+    contentType: 'application/json',
+    dataType: 'jsonp',
+    success: function(cars) {
+      addCarsToDOM(cars);
+    },
+    error: function(response) {
+      $('body').text("Sorry, there was an error with the request. Please refresh the page.")
+    }
+  });
+}
--- public/js/on-click.js
+++ public/js/on-click.js
@@ -7,11 +7,5 @@ $(document).ready(function() {
   // add click listener here
   // it should call on fetchJSON()
     $('button').on('click', fetchJSON);
-      $('button').on('click', updateBaseUrl);
 });
 
-function updateBaseUrl(){
-  console.log(urlRefresh)
-  urlRefresh = `/${parseInt(urlRefresh[1])+1}/3`
-  console.log(urlRefresh)
-}
