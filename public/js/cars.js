9dbeed6981084492757d003ee90d3208a7c520f0
Joe <jsbenning4@gmail.com>
2017-02-21 19:28:15 -0500

    Done.

--- public/js/cars.js
+++ public/js/cars.js
@@ -9,71 +9,42 @@ function formatCars(carsJSON) {
   // to format three cars, each in a div with a class "col-md-4", in a 
   // div with a class "row"
 
-  //var myArray = [];
-  var myCounter = ($("#cars .row .col-md-4").length) + 1;
+  var startingIndex = ($("#cars .row .col-md-4").length);
 
-  if (carsJSON[myCounter] !== undefined) {
-    var car1 = {};
-    car1.make = carsJSON[myCounter]["Make"];
-    car1.model = carsJSON[myCounter]["Model"];
-    car1.year = carsJSON[myCounter]["Year"];
-    //myArray.push(car1);
-  };
+  if (typeof(startingIndex) === "undefined" || !startingIndex) {
+    startingIndex = 0;
+  }
 
-  if (carsJSON[myCounter + 1] !== undefined) {
-    var car2 = {};
-    car2.make = carsJSON[myCounter + 1]["Make"];
-    car2.model = carsJSON[myCounter + 1]["Model"];
-    car2.year = carsJSON[myCounter + 1]["Year"];
-    //myArray.push(car2);
-  };
+  var myString = "";
 
-  if (carsJSON[myCounter + 2] !== undefined) {
-    var car3 = {};
-    car3.make = carsJSON[myCounter + 2]["Make"];
-    car3.model = carsJSON[myCounter + 2]["Model"];
-    car3.year = carsJSON[myCounter + 2]["Year"];
-    //myArray.push(car3);
-  };
+  if (typeof(carsJSON[startingIndex]) !== "undefined") {
+    var car = {};
+    car.make = carsJSON[startingIndex]["Make"];
+    car.model = carsJSON[startingIndex]["Model"];
+    car.year = carsJSON[startingIndex]["Year"];
+    myString = myString.concat("<div class='row'>" + "<div class='col-md-4 car'><h2>" + car.make + "</h2><p><strong>Model:</strong> " + car.model + "</p><p><strong>Year:</strong> " + car.year + "</p></div>");
+  }
 
-  var myPageChunk = 
+  if (typeof(carsJSON[startingIndex + 1]) !== "undefined") {
+    var car = {};
+    car.make = carsJSON[startingIndex + 1]["Make"];
+    car.model = carsJSON[startingIndex + 1]["Model"];
+    car.year = carsJSON[startingIndex + 1]["Year"];
+    myString = myString.concat("<div class='col-md-4 car'><h2>" + car.make + "</h2><p><strong>Model:</strong> " + car.model + "</p><p><strong>Year:</strong> " + car.year + "</p></div>");
+  }  
 
-  `<div class='row'>
-    <div class='col-md-4 car'> 
-      <h2> ${car1.make}</h2>
-      <p><strong>Model:</strong> ${car1.model}</p>
-      <p><strong>Year:</strong> ${car1.year}</p>
-    </div>
+  if (typeof(carsJSON[startingIndex + 2]) !== "undefined") {
+    var car = {};
+    car.make = carsJSON[startingIndex + 2]["Make"];
+    car.model = carsJSON[startingIndex + 2]["Model"];
+    car.year = carsJSON[startingIndex + 2]["Year"];
+    myString = myString.concat("<div class='col-md-4 car'><h2>" + car.make + "</h2><p><strong>Model:</strong> " + car.model + "</p><p><strong>Year:</strong> " + car.year + "</p></div>");
+  } 
 
-    <div class='col-md-4 car'>
-      <h2> ${car2.make}</h2>
-      <p><strong>Model:</strong> ${car2.model}</p>
-      <p><strong>Year:</strong> ${car2.year}</p>
-    </div>
+  myString = myString.concat("</div>");
 
-    <div class='col-md-4 car'>
-      <h2> ${car3.make}</h2>
-      <p><strong>Model:</strong> ${car3.model}</p>
-      <p><strong>Year:</strong> ${car3.year}</p>
-    </div>
-  </div>`;
-
-  return myPageChunk;
-
+  return myString;
 }
-
-
-
-//   function concatenatedHTML(myArray) { 
-//     var addedHTML = `<div class='row'>`;
-//     myArray.forEach(function(car) {    
-//      addedHTML = addedHTML.concat(`<div class='col-md-4 car'> <h2> ${car.make}</h2><p><strong>Model:</strong> ${car.model}</p><p><strong>Year:</strong> ${car.year}</p></div>`);
-//   });
-//     addedHTML = addedHTML.concat(`</div>`);
-//     return addedHTML;
-//   }  
-//   return concatenatedHTML(myArray); 
-// }
 
 
 function addCarsToDOM(carsJSON) {
@@ -84,9 +55,11 @@ function addCarsToDOM(carsJSON) {
 
   if (carsJSON) {
     var myHtml = formatCars(carsJSON);
+    //console.log(myHtml);
     carGroup.append(myHtml);
   };
 }
+
 
 function fetchJSON(event) {   
 // this function will make the ajax call
@@ -95,16 +68,15 @@ function fetchJSON(event) {
 
   var url = "http://mimeocarlisting.azurewebsites.net/api/cars/"
 
-  function logResults(json){
-    console.log(json);
-  }
-
+  // function logResults(json){
+  //   console.log(json);
+  // }
 
   $.ajax({
     url: url,
     contentType: "application/json",
     dataType: "jsonp",
-    jsonpCallback: "logResults",
+    //jsonpCallback: "logResults",
     success: function(data) {
       addCarsToDOM(data);
     },
@@ -115,4 +87,72 @@ function fetchJSON(event) {
     }
   });
 }
+
+
+// function formatCars(carsJSON) {
+//   // this function should return a string of properly formatted html
+//   // refer to app/views/index.erb lines 16 - 22 for an example of how
+//   // to format three cars, each in a div with a class "col-md-4", in a 
+//   // div with a class "row"
+
+//   var myArray = [];
+//   var myCounter = ($("#cars .row .col-md-4").length);
+
+//   for (var i = 0; i < 3; i++ ) {
+//     if (carsJSON[myCounter] !== undefined) {
+//       var car = {};
+//       car.make = carsJSON[myCounter]["Make"];
+//       car.model = carsJSON[myCounter]["Model"];
+//       car.year = carsJSON[myCounter]["Year"];
+//       myArray.push(car);
+//       myCounter = myCounter + 1;
+//     }
+//   };
+
+//   function concatenatedHTML(myArray) { 
+//     var addedHTML = "<div class='row'>";
+//     myArray.forEach(function(car) {    
+//      addedHTML = addedHTML.concat("<div class='col-md-4 car'><h2>" + car.make + "</h2><p><strong>Model:</strong> " + car.model + "</p><p><strong>Year:</strong> " + car.year + "</p></div>");
+//     });
+//     addedHTML = addedHTML.concat("</div>");
+//     return addedHTML;
+//   }
+
+//   return concatenatedHTML(myArray);
+//}
+
+// function formatCars(carsJSON) {
+//   // this function should return a string of properly formatted html
+//   // refer to app/views/index.erb lines 16 - 22 for an example of how
+//   // to format three cars, each in a div with a class "col-md-4", in a 
+//   // div with a class "row"
+
+//   var myArray = [];
+//   var myCounter = 0;
+//   var jsonLength = carsJSON.length;
+//   var trippler = 0;
   
+//   for (var i = 0; i < jsonLength; i++ ) {
+//     if (carsJSON[myCounter] !== undefined) {
+//       var car = {};
+//       car.make = carsJSON[myCounter]["Make"];
+//       car.model = carsJSON[myCounter]["Model"];
+//       car.year = carsJSON[myCounter]["Year"];
+
+//       if (trippler == 0) {
+//         myArray[myCounter] = "<div class='row'>" + "<div class='col-md-4 car'><h2>" + car.make + "</h2><p><strong>Model:</strong> " + car.model + "</p><p><strong>Year:</strong> " + car.year + "</p></div>";
+//         trippler = trippler + 1;
+//       } else if  (trippler == 2) {
+//           myArray[myCounter] = "<div class='col-md-4 car'><h2>" + car.make + "</h2><p><strong>Model:</strong> " + car.model + "</p><p><strong>Year:</strong> " + car.year + "</div>";
+//           trippler = 0;
+//       } else {
+//           myArray[myCounter] = "<div class='col-md-4 car'><h2>" + car.make + "</h2><p><strong>Model:</strong> " + car.model + "</p><p><strong>Year:</strong> " + car.year;
+//           trippler = trippler + 1;
+//       }
+//       myCounter = myCounter + 1;
+//     };
+      
+//   };
+
+//   return myArray.split(" ");
+// }
--- public/js/on-click.js
+++ public/js/on-click.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
   // add click listener here
   // it should call on fetchJSON()
   $(document).on("click", "#load-cars", fetchJSON);
-
 })
 
 
