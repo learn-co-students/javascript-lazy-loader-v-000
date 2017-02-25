15c7573a91fb88700de41c882a89e9c4fb3cb05f
Joe <jsbenning4@gmail.com>
2017-02-25 13:45:43 -0500

    Done.

--- public/js/cars.js
+++ public/js/cars.js
@@ -3,17 +3,17 @@
 // this is the base API url
 var baseUrl = "http://mimeocarlisting.azurewebsites.net/api/cars/";
 
+
 function formatCars(carsJSON) {
-  // this function should return a string of properly formatted html
   // refer to app/views/index.erb lines 16 - 22 for an example of how
   // to format three cars, each in a div with a class "col-md-4", in a 
   // div with a class "row"
 
   var startingIndex = ($("#cars .row .col-md-4").length);
 
-  if (typeof(startingIndex) === "undefined" || !startingIndex) {
-    startingIndex = 0;
-  }
+  // if (typeof(startingIndex) === "undefined" || !startingIndex) {
+  //   startingIndex = 0;
+  // }
 
   var myString = "";
 
@@ -22,7 +22,7 @@ function formatCars(carsJSON) {
     car.make = carsJSON[startingIndex]["Make"];
     car.model = carsJSON[startingIndex]["Model"];
     car.year = carsJSON[startingIndex]["Year"];
-    myString = myString.concat("<div class='row'>" + "<div class='col-md-4 car'><h2>" + car.make + "</h2><p><strong>Model:</strong> " + car.model + "</p><p><strong>Year:</strong> " + car.year + "</p></div>");
+    myString = myString.concat('<div class="row">' + '<div class="col-md-4 car"><h2>' + car.make + '</h2><p><strong>Model:</strong> ' + car.model + '</p><p><strong>Year:</strong> ' + car.year + '</p></div>');
   }
 
   if (typeof(carsJSON[startingIndex + 1]) !== "undefined") {
@@ -30,7 +30,7 @@ function formatCars(carsJSON) {
     car.make = carsJSON[startingIndex + 1]["Make"];
     car.model = carsJSON[startingIndex + 1]["Model"];
     car.year = carsJSON[startingIndex + 1]["Year"];
-    myString = myString.concat("<div class='col-md-4 car'><h2>" + car.make + "</h2><p><strong>Model:</strong> " + car.model + "</p><p><strong>Year:</strong> " + car.year + "</p></div>");
+    myString = myString.concat('<div class="col-md-4 car"><h2>' + car.make + '</h2><p><strong>Model:</strong> ' + car.model + '</p><p><strong>Year:</strong> ' + car.year + '</p></div>');
   }  
 
   if (typeof(carsJSON[startingIndex + 2]) !== "undefined") {
@@ -38,10 +38,10 @@ function formatCars(carsJSON) {
     car.make = carsJSON[startingIndex + 2]["Make"];
     car.model = carsJSON[startingIndex + 2]["Model"];
     car.year = carsJSON[startingIndex + 2]["Year"];
-    myString = myString.concat("<div class='col-md-4 car'><h2>" + car.make + "</h2><p><strong>Model:</strong> " + car.model + "</p><p><strong>Year:</strong> " + car.year + "</p></div>");
+    myString = myString.concat('<div class="col-md-4 car"><h2>' + car.make + '</h2><p><strong>Model:</strong> ' + car.model + '</p><p><strong>Year:</strong> ' + car.year + '</p></div>');
   } 
 
-  myString = myString.concat("</div>");
+  myString = myString.concat('</div>');
 
   return myString;
 }
@@ -55,7 +55,11 @@ function addCarsToDOM(carsJSON) {
 
   if (carsJSON) {
     var myHtml = formatCars(carsJSON);
-    //console.log(myHtml);
+    var myCars = document.getElementById("cars");
+    var l1 =  myCars.children.length;
+    var l2 = myCars.last.children.length;
+    console.log(l1 + "," + l2);
+
     carGroup.append(myHtml);
   };
 }
@@ -68,9 +72,9 @@ function fetchJSON(event) {
 
   var url = "http://mimeocarlisting.azurewebsites.net/api/cars/"
 
-  // function logResults(json){
-  //   console.log(json);
-  // }
+  function logResults(json){
+    console.log(json);
+  }
 
   $.ajax({
     url: url,
@@ -80,79 +84,11 @@ function fetchJSON(event) {
     success: function(data) {
       addCarsToDOM(data);
     },
-    error: function(xhr,status,error) {
-      console.log(error);
-      alert("fail");
-      $('#msg').html("ERROR...! Please check the console (F12)");
+    error: function(xhr, status, error) {
+      //console.log(error);
+      alert("Something failed!");
     }
   });
 }
 
 
-// function formatCars(carsJSON) {
-//   // this function should return a string of properly formatted html
-//   // refer to app/views/index.erb lines 16 - 22 for an example of how
-//   // to format three cars, each in a div with a class "col-md-4", in a 
-//   // div with a class "row"
-
-//   var myArray = [];
-//   var myCounter = ($("#cars .row .col-md-4").length);
-
-//   for (var i = 0; i < 3; i++ ) {
-//     if (carsJSON[myCounter] !== undefined) {
-//       var car = {};
-//       car.make = carsJSON[myCounter]["Make"];
-//       car.model = carsJSON[myCounter]["Model"];
-//       car.year = carsJSON[myCounter]["Year"];
-//       myArray.push(car);
-//       myCounter = myCounter + 1;
-//     }
-//   };
-
-//   function concatenatedHTML(myArray) { 
-//     var addedHTML = "<div class='row'>";
-//     myArray.forEach(function(car) {    
-//      addedHTML = addedHTML.concat("<div class='col-md-4 car'><h2>" + car.make + "</h2><p><strong>Model:</strong> " + car.model + "</p><p><strong>Year:</strong> " + car.year + "</p></div>");
-//     });
-//     addedHTML = addedHTML.concat("</div>");
-//     return addedHTML;
-//   }
-
-//   return concatenatedHTML(myArray);
-//}
-
-// function formatCars(carsJSON) {
-//   // this function should return a string of properly formatted html
-//   // refer to app/views/index.erb lines 16 - 22 for an example of how
-//   // to format three cars, each in a div with a class "col-md-4", in a 
-//   // div with a class "row"
-
-//   var myArray = [];
-//   var myCounter = 0;
-//   var jsonLength = carsJSON.length;
-//   var trippler = 0;
-  
-//   for (var i = 0; i < jsonLength; i++ ) {
-//     if (carsJSON[myCounter] !== undefined) {
-//       var car = {};
-//       car.make = carsJSON[myCounter]["Make"];
-//       car.model = carsJSON[myCounter]["Model"];
-//       car.year = carsJSON[myCounter]["Year"];
-
-//       if (trippler == 0) {
-//         myArray[myCounter] = "<div class='row'>" + "<div class='col-md-4 car'><h2>" + car.make + "</h2><p><strong>Model:</strong> " + car.model + "</p><p><strong>Year:</strong> " + car.year + "</p></div>";
-//         trippler = trippler + 1;
-//       } else if  (trippler == 2) {
-//           myArray[myCounter] = "<div class='col-md-4 car'><h2>" + car.make + "</h2><p><strong>Model:</strong> " + car.model + "</p><p><strong>Year:</strong> " + car.year + "</div>";
-//           trippler = 0;
-//       } else {
-//           myArray[myCounter] = "<div class='col-md-4 car'><h2>" + car.make + "</h2><p><strong>Model:</strong> " + car.model + "</p><p><strong>Year:</strong> " + car.year;
-//           trippler = trippler + 1;
-//       }
-//       myCounter = myCounter + 1;
-//     };
-      
-//   };
-
-//   return myArray.split(" ");
-// }
