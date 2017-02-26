d07fc0fe30c0aead8beafd74032b1c0190b54519
Joe <jsbenning4@gmail.com>
2017-02-25 22:46:01 -0500

    Done.

--- public/js/cars.js
+++ public/js/cars.js
@@ -2,47 +2,45 @@
 
 // this is the base API url
 var baseUrl = "http://mimeocarlisting.azurewebsites.net/api/cars/";
-
+var dataIndex = 6;
+var dataArray = [];
+//var testr = [{"Make":"Dodge","Model":"Avenger","Year":"2013"},{"Make":"Nissan","Model":"Maxima","Year":"2009"},{"Make":"Subaru","Model":"Impreza WRX","Year":"2013"}];
 
 function formatCars(carsJSON) {
   // refer to app/views/index.erb lines 16 - 22 for an example of how
   // to format three cars, each in a div with a class "col-md-4", in a 
   // div with a class "row"
 
-  var startingIndex = ($("#cars .row .col-md-4").length);
-
-  // if (typeof(startingIndex) === "undefined" || !startingIndex) {
-  //   startingIndex = 0;
-  // }
 
   var myString = "";
+  //console.log(carsJSON[startingIndex]);
 
-  if (typeof(carsJSON[startingIndex]) !== "undefined") {
+  if (typeof(carsJSON[0]) !== "undefined") {
     var car = {};
-    car.make = carsJSON[startingIndex]["Make"];
-    car.model = carsJSON[startingIndex]["Model"];
-    car.year = carsJSON[startingIndex]["Year"];
+    car.make = carsJSON[0]["Make"];
+    car.model = carsJSON[0]["Model"];
+    car.year = carsJSON[0]["Year"];
     myString = myString.concat('<div class="row">' + '<div class="col-md-4 car"><h2>' + car.make + '</h2><p><strong>Model:</strong> ' + car.model + '</p><p><strong>Year:</strong> ' + car.year + '</p></div>');
   }
 
-  if (typeof(carsJSON[startingIndex + 1]) !== "undefined") {
+  if (typeof(carsJSON[1]) !== "undefined") {
     var car = {};
-    car.make = carsJSON[startingIndex + 1]["Make"];
-    car.model = carsJSON[startingIndex + 1]["Model"];
-    car.year = carsJSON[startingIndex + 1]["Year"];
+    car.make = carsJSON[1]["Make"];
+    car.model = carsJSON[1]["Model"];
+    car.year = carsJSON[1]["Year"];
     myString = myString.concat('<div class="col-md-4 car"><h2>' + car.make + '</h2><p><strong>Model:</strong> ' + car.model + '</p><p><strong>Year:</strong> ' + car.year + '</p></div>');
   }  
 
-  if (typeof(carsJSON[startingIndex + 2]) !== "undefined") {
+  if (typeof(carsJSON[2]) !== "undefined") {
     var car = {};
-    car.make = carsJSON[startingIndex + 2]["Make"];
-    car.model = carsJSON[startingIndex + 2]["Model"];
-    car.year = carsJSON[startingIndex + 2]["Year"];
+    car.make = carsJSON[2]["Make"];
+    car.model = carsJSON[2]["Model"];
+    car.year = carsJSON[2]["Year"];
     myString = myString.concat('<div class="col-md-4 car"><h2>' + car.make + '</h2><p><strong>Model:</strong> ' + car.model + '</p><p><strong>Year:</strong> ' + car.year + '</p></div>');
   } 
 
   myString = myString.concat('</div>');
-
+  dataArray = [];
   return myString;
 }
 
@@ -55,11 +53,6 @@ function addCarsToDOM(carsJSON) {
 
   if (carsJSON) {
     var myHtml = formatCars(carsJSON);
-    var myCars = document.getElementById("cars");
-    var l1 =  myCars.children.length;
-    var l2 = myCars.last.children.length;
-    console.log(l1 + "," + l2);
-
     carGroup.append(myHtml);
   };
 }
@@ -82,7 +75,16 @@ function fetchJSON(event) {
     dataType: "jsonp",
     //jsonpCallback: "logResults",
     success: function(data) {
-      addCarsToDOM(data);
+      var counter = dataIndex;
+      for (var i = 0; i < 3; i++) {
+        if (data[counter]) {
+          dataArray.push(data[counter]);
+          counter += 1;
+        };
+      } 
+      dataIndex = dataIndex + (dataArray.length);
+      console.log(dataArray);
+      addCarsToDOM(dataArray);
     },
     error: function(xhr, status, error) {
       //console.log(error);
@@ -91,4 +93,96 @@ function fetchJSON(event) {
   });
 }
 
+
+
+
+
+// "use strict";
+
+// // this is the base API url
+// var baseUrl = "http://mimeocarlisting.azurewebsites.net/api/cars/";
+
+// function formatCars(carsJSON) {
+//   // refer to app/views/index.erb lines 16 - 22 for an example of how
+//   // to format three cars, each in a div with a class "col-md-4", in a 
+//   // div with a class "row"
+
+
+
+//   var startingIndex = ($("#cars .row .col-md-4").length);
+
+//   // if (typeof(startingIndex) === "undefined" || !startingIndex) {
+//   //   startingIndex = 0;
+//   // }
+
+//   var myString = "";
+//   //console.log(carsJSON[startingIndex]);
+
+//   if (typeof(carsJSON[startingIndex]) !== "undefined") {
+//     var car = {};
+//     car.make = carsJSON[startingIndex]["Make"];
+//     car.model = carsJSON[startingIndex]["Model"];
+//     car.year = carsJSON[startingIndex]["Year"];
+//     myString = myString.concat('<div class="row">' + '<div class="col-md-4 car"><h2>' + car.make + '</h2><p><strong>Model:</strong> ' + car.model + '</p><p><strong>Year:</strong> ' + car.year + '</p></div>');
+//   }
+
+//   if (typeof(carsJSON[startingIndex + 1]) !== "undefined") {
+//     var car = {};
+//     car.make = carsJSON[startingIndex + 1]["Make"];
+//     car.model = carsJSON[startingIndex + 1]["Model"];
+//     car.year = carsJSON[startingIndex + 1]["Year"];
+//     myString = myString.concat('<div class="col-md-4 car"><h2>' + car.make + '</h2><p><strong>Model:</strong> ' + car.model + '</p><p><strong>Year:</strong> ' + car.year + '</p></div>');
+//   }  
+
+//   if (typeof(carsJSON[startingIndex + 2]) !== "undefined") {
+//     var car = {};
+//     car.make = carsJSON[startingIndex + 2]["Make"];
+//     car.model = carsJSON[startingIndex + 2]["Model"];
+//     car.year = carsJSON[startingIndex + 2]["Year"];
+//     myString = myString.concat('<div class="col-md-4 car"><h2>' + car.make + '</h2><p><strong>Model:</strong> ' + car.model + '</p><p><strong>Year:</strong> ' + car.year + '</p></div>');
+//   } 
+
+//   myString = myString.concat('</div>');
+//   dataArray = [];
+//   return myString;
+// }
+
+
+// function addCarsToDOM(carsJSON) {
+//   // this function should pass carsJSON to formatCars() and then 
+//   // add the resulting HTML to the div with an id of "cars"
+
+//   var carGroup = $("#cars");
+
+//   if (carsJSON) {
+//     var myHtml = formatCars(carsJSON);
+//     carGroup.append(myHtml);
+//   };
+// }
+
+// function fetchJSON(event) {   
+// // this function will make the ajax call
+// // on success of the ajax call, it will pass the returned data
+// // to addCarsToDOM()
+
+//   var url = "http://mimeocarlisting.azurewebsites.net/api/cars/"
+
+//   function logResults(json){
+//     console.log(json);
+//   }
+
+//   $.ajax({
+//     url: url,
+//     contentType: "application/json",
+//     dataType: "jsonp",
+//     //jsonpCallback: "logResults",
+//     success: function(data) {
+//       addCarsToDOM(data);
+//     },
+//     error: function(xhr, status, error) {
+//       //console.log(error);
+//       alert("Something failed!");
+//     }
+//   });
+// }
 
