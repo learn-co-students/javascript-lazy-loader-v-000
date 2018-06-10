c2a36fcc1d83b6c4ad9f9da1c4b99c98fc349cac
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2018-06-10 00:03:47 +0000

    Done.

--- public/js/cars.js
+++ public/js/cars.js
@@ -1,40 +1,34 @@
 "use strict";
 
 // this is the base API url
-var baseUrl = "https://mimeocarlisting.azurewebsites.net/api/cars/",
-counter = 2;
-
+var pageNum = 2;
+var baseUrl = "https://mimeocarlisting.azurewebsites.net/api/cars/";
 
 function formatCars(carsJSON) {
   // this function shold return a string of properly formatted html
   // refer to app/views/index.erb lines 16 - 22 for an example of how
   // to format three cars, each in a div with a class "col-md-4", in a 
   // div with a class "row"
-  var html = `<div class="row">`;var i=0;
- if (counter > 2) {
-   i = counter;
- }
+var html = "<div class=\"row\">";
 
-  while(i<=counter){
-    html+=`<div class="col-md-4 car">`;
-    html+='<h2>'+carsJSON[i]["Make"]+'</h2>';
-    html+='<p><strong>Model:</strong> '+carsJSON[i]["Model"]+'</p>';
-    html+='<p><strong>Year:</strong> '+carsJSON[i]["Year"]+'</p>';
+$.each(carsJSON, function(index, car){
+    html+= "<div class=\"col-md-4 car\">";
+    html+='<h2>'+car.Make+"</h2>";
+    html+='<p><strong>Model:</strong> '+car.Model+'</p>';
+    html+='<p><strong>Year:</strong> '+car.Year+'</p>';
     html+='</div>';
-    i++;
-  }
-
+  });
 
   html += '</div>';
-  counter = counter + 3;
+
   return html;
 }
 
 function addCarsToDOM(carsJSON) {
   // this function should pass carsJSON to formatCars() and then 
   // add the resulting HTML to the div with an id of "cars"
-  
-  $( "#cars .row" ).append( $( formatCars(carsJSON) ) );
+  var html = formatCars(carsJSON);
+  $( "#cars" ).append(  html  );
   
 }
 
@@ -42,10 +36,11 @@ function fetchJSON() {
   // this function will make the ajax call
   // on success of the ajax call, it will pass the returned data
   // to addCarsToDOM()
-  
+pageNum ++;
+
 $.ajax({
-        url: baseUrl,
-        contentType: 'application/x-www-form-urlencoded',
+        url: baseUrl+pageNum+"/3",
+        contentType: 'application/json',
         dataType: 'jsonp',
         success: function(carsJSON) {
           addCarsToDOM(carsJSON);
